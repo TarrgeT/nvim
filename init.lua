3eccb04b5bba9a7db5e7ee9c0de01baf1135a5ad
TarrgeT <garrettormsby9@gmail.com>
2026-02-05 21:03:03 -0600

    relative

--- init.lua
+++ init.lua
@@ -68,6 +68,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
+vim.opt.relativenumber = true
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
